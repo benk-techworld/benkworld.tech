1850675038b1863fb6c5651b15011e7ec782c9c0
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-05-03 14:35:55 +0100

    Create vite react-ts app

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">
+  Benk Techworld - Frontend Application
+</h1>
+
+<p align="center">
+  Written in <a href="https://www.typescriptlang.org/" target="_blank">Typescript</a> using <a href="https://vitejs.dev/" target="_blank">Vite</a> and <a href="https://react.dev/" target="_blank">React</a>
+</p>
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <h1>Under Construction</h1>
+    </>
+  )
+}
+
+export default App
