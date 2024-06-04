c32ac0e2bc41fdfc34597d7eaf451b9f125bb65a
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-06-04 21:42:52 +0100

    modified theme toggle button

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import {useTheme} from "@/hooks/useTheme"
-import {Button} from "@nextui-org/react"
+// import {Button} from "@nextui-org/react"
 import { IoIosMoon } from "react-icons/io";
 import { MdSunny } from "react-icons/md";
 
@@ -8,8 +8,8 @@ export default function ThemeSwitcher() {
   const {theme,setTheme} = useTheme();
   
   return (
-      <Button onPress={()=>setTheme(theme==="dark"? "light":"dark")} isIconOnly size="sm" className="bg-transparent inline-block" aria-label="Like">
-        {theme === "dark" ? <MdSunny className="text-secondary" size={21}/> : <IoIosMoon className="text-secondary" size={21}/>}
-      </Button>      
+      <button onClick={()=>setTheme(theme==="dark"? "light":"dark")} className="bg-transparent rounded">
+        {theme === "dark" ? <MdSunny className="text-secondary" size={23}/> : <IoIosMoon className="text-secondary" size={23}/>}
+      </button>      
   );
 }
