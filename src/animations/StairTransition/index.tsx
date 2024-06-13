8ed2b2b4f79f0c6d86d183ac18e62a9f35bc1fca
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-06-13 15:04:10 +0100

    smoothing animation

--- src/animations/StairTransition/index.tsx
+++ src/animations/StairTransition/index.tsx
@@ -10,13 +10,16 @@ const StairTransition = () => {
     return (
         <AnimatePresence mode='wait'>
             <motion.div key={pathname}> 
-                <div className='h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-[1000] flex'>
+                <div className='h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-50 flex'>
                     <Stairs/>
                 </div>
                 <motion.div
-                    className='h-screen w-screen fixed bg-background top-0 pointer-events-none'
-                    initial={{opacity: 0}}
-                    animate={{opacity:0,transition:{delay:1,duration:0.4,ease:"easeInOut"}}}
+                    className='h-screen w-screen fixed bg-background top-0 pointer-events-none z-40'
+                    initial={{opacity: 1}}
+                    animate={{
+                        opacity:0,
+                        transition: {delay: 1, duration: 0.4, ease: "easeInOut"}
+                    }}
 
                 />
             </motion.div>
